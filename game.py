6290643394daf7ac7551c4ca43e77f3df27cf412
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-10-19 13:07:44 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,16 @@
 # !python3
 
 import pyautogui as p
+import time as t
 
+doLoop = True
+count = 0
+while doLoop:
+    p.click(1118,470)
+    count = count + 1
+    if p.pixelMatchesColor(1691,896,(102,255,102)) == True:
+        p.click(1691,896)
+    if count == 50:
+        count = 0
+        p.click()
+        doLoop = False
