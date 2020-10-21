6f0db48050cc52dd0ac2bca914dc48eb5eeb5df0
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-10-21 12:42:25 -0700

    Update game.py

--- game.py
+++ game.py
@@ -5,18 +5,18 @@ import time as t
 
 doLoop = True
 count = 0
-cursor = 0
-
+a = 0
 while doLoop:
     p.click(1118,470)
     count = count + 1
-    if count == 100:
+    a = a + 1
+    if count == 150:
         count = 0
         while p.pixelMatchesColor(1650,533,(16,40,54),tolerance=8):
             p.click(1650,533)
             #upgrade
-        while p.pixelMatchesColor(1674,999,(255,255,255),tolerance = 10):
-            p.click(1674,999)
+        while p.pixelMatchesColor(1674,995,(255,255,255),tolerance = 10):
+            p.click(1674,995)
         while p.pixelMatchesColor(1674,939,(255,255,255),tolerance = 10):
             p.click(1674,939)
         while p.pixelMatchesColor(1674,872,(255,255,255),tolerance = 10):
@@ -29,7 +29,7 @@ while doLoop:
             p.click(1672,681)
         while p.pixelMatchesColor(1673,620,(255,255,255),tolerance = 10):
             p.click(1673,620)
-            cursor = cursor +1
-            if cursor == 50:
-                break
-        
+    if a == 300:
+        a = 0
+        if p.confirm(text='Stop the program?',title='',buttons=['Stop','Continue']) == 'Stop':
+            doLoop = False
