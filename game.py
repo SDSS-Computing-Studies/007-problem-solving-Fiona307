a77efd24583a3efaaa9c22718e32f5d150722fa5
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-10-22 13:02:51 -0700

    Update game.py

--- game.py
+++ game.py
@@ -12,22 +12,22 @@ while doLoop:
     a = a + 1
     if count == 150:
         count = 0
-        while p.pixelMatchesColor(1650,533,(16,40,54),tolerance=8):
+        while p.pixelMatchesColor(1650,533,(16,40,54),tolerance= 0.9):
             p.click(1650,533)
             #upgrade
-        while p.pixelMatchesColor(1674,995,(255,255,255),tolerance = 10):
+        while p.pixelMatchesColor(1674,995,(255,255,255),tolerance = 0.9):
             p.click(1674,995)
-        while p.pixelMatchesColor(1674,939,(255,255,255),tolerance = 10):
+        while p.pixelMatchesColor(1674,939,(255,255,255),tolerance = 0.9):
             p.click(1674,939)
-        while p.pixelMatchesColor(1674,872,(255,255,255),tolerance = 10):
+        while p.pixelMatchesColor(1674,872,(255,255,255),tolerance = 0.9):
             p.click(1674,872)
-        while p.pixelMatchesColor(1674,811,(255,255,255),tolerance = 10):
+        while p.pixelMatchesColor(1674,811,(255,255,255),tolerance = 0.9):
             p.click(1674,811)
-        while p.pixelMatchesColor(1674,745,(255,255,255),tolerance = 10):
+        while p.pixelMatchesColor(1674,745,(255,255,255),tolerance = 0.9):
             p.click(1674,745)
-        while p.pixelMatchesColor(1672,681,(255,255,255),tolerance = 10):
+        while p.pixelMatchesColor(1672,681,(255,255,255),tolerance = 0.9):
             p.click(1672,681)
-        while p.pixelMatchesColor(1673,620,(255,255,255),tolerance = 10):
+        while p.pixelMatchesColor(1673,620,(255,255,255),tolerance = 0.9):
             p.click(1673,620)
     if a == 600:
         a = 0
